a60d9bd59bbf98484cad873a27efe480b125b58f
andrew mclagan <andrewmclagan@gmail.com>
2018-05-30 22:12:42 +1000

    Presenters

---
+++ src/Presenters/Presenter.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace EthicalJobs\Quantify\Presenters;
+
+/**
+ * Formats report data for display
+ *
+ * @author Andrew McLagan
+ */
+
+class Presenter
+{
+	/**
+	 * Transforms report data
+	 * 
+	 * @param  array  $input
+	 * @return mixed
+	 */
+	public function transform(array $input);
+}
